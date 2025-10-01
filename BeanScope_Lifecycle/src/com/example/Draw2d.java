57eb1d3187426520799abe065bac9f1598bb5bf0
RahmaIsmaill <rahmaismail2010@gmail.com>
2025-10-01 21:01:57 +0300

    bs

--- BeanScope_Lifecycle/src/com/example/Draw2d.java
+++ BeanScope_Lifecycle/src/com/example/Draw2d.java
@@ -1,6 +1,7 @@
 package com.example;
 
-public class Draw2d {
+public class Draw2d implements  Draw {
+    @Override
     public void draw(String shapeName) {
         System.out.println("Drawing a 2d for " + shapeName);
     }
